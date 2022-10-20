5e08a76cd5b95f4b3f6a9b54902fbf56fa3bcb06
JosejCruz <jcruz@medicaresoftsrl.com>
2022-10-20 18:18:41 -0400

    vmobile v1.10
    agregando selects a estado y lista de estudios

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,10 +1,16 @@
-import { IonContent, IonHeader, IonItem, IonLabel, IonList, IonMenu, IonTitle, IonToolbar } from '@ionic/react'
+import { IonAccordion, IonAccordionGroup, IonButton, IonContent, IonFooter, IonHeader, IonIcon, IonItem, IonLabel, IonList, IonMenu, IonSelect, IonSelectOption, IonTitle, IonToolbar } from '@ionic/react'
 import React from 'react'
 interface Listprops{
   'lista': any
 }
 function Item(props: Listprops) {
   console.log(props.lista)
+  const handleButtonSave = () =>{
+    console.log('guardar id: ' + props.lista._id)
+  }
+  const handleButtonCancel = () =>{
+    console.log('cancelar id: ' + props.lista._id)
+  }
   return (
     <IonMenu className='ancho' side="end" contentId={props.lista._id}>
       <IonHeader >
@@ -14,6 +20,13 @@ function Item(props: Listprops) {
       </IonHeader>
       <IonContent>
         <IonList>
+          <IonItem>
+            <IonLabel>Estado</IonLabel>
+          <IonSelect interface='popover' placeholder='Estado'>
+            <IonSelectOption value={'Preregistro'}>Pre-registro</IonSelectOption>
+            <IonSelectOption value={'Ingresado'}>Ingresado</IonSelectOption>
+          </IonSelect>
+          </IonItem>
           <IonItem>Estado = {props.lista.Tipo}</IonItem>
           <IonItem>Nombre: = {props.lista.Paciente.nombre}</IonItem>
           <IonList>
@@ -28,8 +41,26 @@ function Item(props: Listprops) {
             )
           })}
           </IonList>
+            <IonAccordionGroup>
+              <IonAccordion value='Estudios'>
+                <IonItem slot='header' color={'light'}>
+                  <IonLabel>Lista de Estudios</IonLabel>
+                </IonItem>
+                {props.lista.ListaEstudios.map((contenido:any, index:any)=>{
+                  return(
+                    <div key={index} className='ion-padding' slot='content'>
+                      {contenido.nombreMod}
+                    </div>
+                  )
+                })}
+              </IonAccordion>
+            </IonAccordionGroup>
         </IonList>
       </IonContent>
+      <IonFooter>
+        <IonButton onClick={handleButtonSave} expand='block' color={'primary'}> <IonIcon slot='start' icon='save-outline'></IonIcon> Guardar</IonButton>
+        <IonButton onClick={handleButtonCancel} expand='block' color={'danger'}> <IonIcon slot='start' icon='close-circle-outline'></IonIcon> Cancelar</IonButton>
+      </IonFooter>
     </IonMenu>
   )
 }
